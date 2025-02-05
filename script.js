af55bf28eeea62c4896ac5ba3571821d8866c819
LimboLam <liam.xiong24@student.cart.org>
2025-02-05 09:50:17 -0800

    asd

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const certificate = document.getElementById(`certificates`);
+const certificate = document.querySelector(`img`);
 const button = document.querySelector(`button`);
 
 function changeImage() {
-    button.addEventListener(`click`, function() {
-        if (certificate.src === `images/certificate_1.png`) {
-          certificate.src = `images/certificate_2.png`;
-        } else if (certificate.src === `images/certificate_2.png`) {
-            certificate.src = `images/certificate_3.png`;
-        } else {
-            certificate.src = `images/certificate_1.png`;
-        }
-    });
+    if (certificate.src === `images/certificate_1.png`) {
+        certificate.src = `images/certificate_2.png`;
+    } else if (certificate.src === `images/certificate_2.png`) {
+        certificate.src = `images/certificate_3.png`;
+    } else {
+        certificate.src = `images/certificate_1.png`;
+    }
 }
+
+button.addEventListener(`click`, function() {
+    changeImage()
+});
