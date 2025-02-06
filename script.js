bd2e27ee8ea46ea73f2989c038c7d4aa27cf124d
LimboLam <liam.xiong24@student.cart.org>
2025-02-06 08:57:31 -0800

    stuff

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const certificate = document.querySelector(`img`);
 const button = document.querySelector(`button`);
 
 function changeImage() {
-    if (certificate.src === `images/certificate_1.png`) {
-        certificate.src = `images/certificate_2.png`;
-    } else if (certificate.src === `images/certificate_2.png`) {
-        certificate.src = `images/certificate_3.png`;
+    let image = certificate.getAttribute(`src`)
+    if (image === `images/certificate_1.png`) {
+        certificate.setAttribute(`src`, `images/certificate_2.png`);
+    } else if (image === `images/certificate_2.png`) {
+        certificate.setAttribute(`src`, `images/certificate_3.png`);
     } else {
-        certificate.src = `images/certificate_1.png`;
+        certificate.setAttribute(`src`, `images/certificate_1.png`);
     }
 }
 
